3693f50029fcb655a6fe02c07c42d4607efc04d7
MSAugdal <mathias.s.augdal@gmail.com>
2024-08-05 18:11:07 +0200

    commiting changes

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,6 +5,8 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+      -- lint.linters_by_ft = lint.linters_by_ft or {}
+      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
       lint.linters_by_ft = {
         -- markdown = { 'markdownlint' },
         -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -19,7 +21,7 @@ return {
         inko = { 'inko' },
         janet = { 'janet' },
         json = { 'jsonlint' },
-        markdown = { 'vale' },
+        -- markdown = { 'vale' },
         rst = { 'vale' },
         ruby = { 'ruby' },
         terraform = { 'tflint' },
